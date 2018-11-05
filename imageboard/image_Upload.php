2acfcc0f74762c180e78971e7b53e399090093e4
alstm1597 <alstm1597@gmail.com>
2018-11-05 19:07:23 +0900

    1105

---
+++ imageboard/image_Upload.php
@@ -0,0 +1,17 @@
+<?php 
+		require_once("../tools.php");
+		require_once("image_BoardDao.php");
+
+		$title = requestValue("title");
+		$content = requestValue("content");
+
+		if($title && $content){
+			//DB에 insert
+			$dao = new ImageBoardDao();
+			$dao->insertMsg($title, $content);
+			okGo("정상적으로 입력되었습니다.", "image_Board.php");
+		}else{
+			errorBack("다시 입력하십시오.");
+		}
+
+ ?>
